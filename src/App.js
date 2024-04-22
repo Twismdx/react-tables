9f4a0430fd5abe66160f0509e251b2815fc6fd4f
twismdx <twism@live.com>
2024-04-23 02:23:02 +0930

    a

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Table16 from './Table16.js'
+import Table17 from './Table16.js'
+import Table18 from './Table16.js'
+import Table19 from './Table16.js'
+import Table20 from './Table16.js'
+import Table21 from './Table16.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Switch>
+        <Route path="/table16" component={Table16} />
+        <Route path="/table17" component={Table17} />
+        <Route path="/table18" component={Table18} />
+        <Route path="/table19" component={Table19} />
+        <Route path="/table20" component={Table20} />
+        <Route path="/table21" component={Table21} />
+        {/* Add more routes if you have more versions */}
+      </Switch>
+    </Router>
+  )
 }
 
-export default App;
+export default App
