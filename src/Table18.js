6be999f46eb3b46a532a78b5ce4f4aec7a518c6f
twismdx <twism@live.com>
2024-04-25 18:14:24 +0930

    a

--- src/Table18.js
+++ src/Table18.js
@@ -3,7 +3,7 @@ import './home.css'
 import io from 'socket.io-client'
 import axios from 'axios'
 
-const socket = io()
+const socket = io('https://twism.vercel.app:4000')
 
 const Table18 = () => {
 	const [org, setOrg] = useState('ko')
