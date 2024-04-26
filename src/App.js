70c7c186c540f6424b625f902e8866c9777f7acf
twismdx <twism@live.com>
2024-04-27 00:06:50 +0930

    a

--- src/App.js
+++ src/App.js
@@ -6,28 +6,20 @@ import Table18 from './Table18.js'
 import Table19 from './Table19.js'
 import Table20 from './Table20.js'
 import Table21 from './Table21.js'
-import * as Ably from 'ably'
-import { AblyProvider } from 'ably/react'
-
-const client = new Ably.Realtime({ key: 'Yo4kvQ.jqBbLQ:d91k3xHWDPnw7T8kmH5qIMrbsNg75pw0L3CrU_8pFdQ', clientId: 'web' })
 
 function App() {
   return (
-    <AblyProvider client={client}>
-      <ChannelProvider channelName='start'>
-        <Router>
-          <Routes>
-            <Route path="/table16" element={<Table16 />} />
-            <Route path="/table17" element={<Table17 />} />
-            <Route path="/table18" element={<Table18 />} />
-            <Route path="/table19" element={<Table19 />} />
-            <Route path="/table20" element={<Table20 />} />
-            <Route path="/table21" element={<Table21 />} />
-            {/* Add more routes if you have more versions */}
-          </Routes>
-        </Router>
-      </ChannelProvider>
-    </AblyProvider>
+    <Router>
+      <Routes>
+        <Route path="/table16" element={<Table16 />} />
+        <Route path="/table17" element={<Table17 />} />
+        <Route path="/table18" element={<Table18 />} />
+        <Route path="/table19" element={<Table19 />} />
+        <Route path="/table20" element={<Table20 />} />
+        <Route path="/table21" element={<Table21 />} />
+        {/* Add more routes if you have more versions */}
+      </Routes>
+    </Router>
   )
 }
 
