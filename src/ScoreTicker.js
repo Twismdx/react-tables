1abae3eb0e42c7583a1eba8cc16a30a291de484f
Damien <80352555+Twismdx@users.noreply.github.com>
2024-07-06 11:34:06 +0000

    a

--- src/ScoreTicker.js
+++ src/ScoreTicker.js
@@ -9,11 +9,25 @@ const ScoreTicker = ({ matches }) => {
                 <div className="ticker">
                     {matches.map((match, index) => (
                         <div className="ticker-item" key={index}>
-                            <span style={{ color: match.home.framescore > match.away.framescore ? 'red' : 'inherit' }}>
+                            <span
+                                style={{
+                                    color:
+                                        match.home.framescore > match.away.framescore
+                                            ? 'red'
+                                            : 'inherit',
+                                }}
+                            >
                                 {`${match.home.shortname} ${match.home.framescore}`}
                             </span>
                             {' - '}
-                            <span style={{ color: match.away.framescore > match.home.framescore ? 'blue' : 'inherit' }}>
+                            <span
+                                style={{
+                                    color:
+                                        match.away.framescore > match.home.framescore
+                                            ? 'blue'
+                                            : 'inherit',
+                                }}
+                            >
                                 {`${match.away.framescore} ${match.away.shortname}`}
                             </span>
                         </div>
